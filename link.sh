58ba8c06f73cde9ebd43113db00376eeab7506f8
baltoli <bsc28@cam.ac.uk>
2017-02-20 13:31:25 +0000

    Implement linking script

---
+++ link.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+FILES=(vimrc tmux.conf)
+
+for f in ${FILES[@]}
+do
+  DOTFILE="$HOME/.$f"
+
+  echo "Removing old file..."
+  rm $DOTFILE
+  echo "Linking $f..."
+  ln -s $f $DOTFILE
+done

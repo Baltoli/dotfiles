65d968776e64c9eb2fc043b4af841f685f524cbd
Bruce Collie <brucecollie82@gmail.com>
2019-10-23 11:53:47 +0100

    Install pypy

--- install-packages.sh
+++ install-packages.sh
@@ -22,7 +22,8 @@ brew install \
         cmake \
         ninja \
         openblas \
-        ghc
+        ghc \
+        pypy3
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip install --user Pygments

1018707766e377c39552acb8d35ee4f734d30ad1
Bruce Collie <brucecollie82@gmail.com>
2019-10-23 09:40:59 +0100

    Update list of macos packages to install on new machine

---
+++ install-packages.sh
@@ -0,0 +1,20 @@
+brew cask install \
+	iterm2 \
+	google-chrome \
+	backblaze \
+	bettertouchtool \
+	spotify \
+	whatsapp \
+	dropbox \
+	caffeine \
+	flux \
+	zotero \
+        skim \
+        mactex
+
+brew install \
+	git \
+	nvim \
+	gcc \
+        cmake \
+        ninja

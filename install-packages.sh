d2f629a473e48f8550182d9c626af01cd6ca981b
Bruce Collie <brucecollie82@gmail.com>
2019-11-06 13:46:09 +0000

    Brewfile

--- install-packages.sh
+++ install-packages.sh
@@ -1,44 +1,6 @@
 brew update
 brew upgrade
-
-brew cask install \
-	iterm2 \
-	google-chrome \
-	backblaze \
-	bettertouchtool \
-	spotify \
-        spotify-notifications \
-	whatsapp \
-	dropbox \
-	caffeine \
-	flux \
-	zotero \
-        skim \
-        mactex \
-        daisydisk \
-        tunnelblick \
-        private-internet-access \
-        gitkraken \
-        gimp
-
-brew install \
-	git \
-	nvim \
-	gcc \
-        cmake \
-        ninja \
-        openblas \
-        ghc \
-        pypy3 \
-        pypy \
-        conan \
-        clang-format \
-        the_silver_searcher \
-        ccache \
-        coreutils \
-        bash \
-        graphviz \
-        cloc
+brew bundle
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip install --user Pygments

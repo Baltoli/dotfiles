316c3c6496c2acbb5f54e3df9f1293b99ffd89dd
Bruce Collie <brucecollie82@gmail.com>
2021-02-18 15:18:14 +0000

    More klee deps

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,9 @@ brew bundle
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip install --user Pygments
+python3 -m pip install --user lit
+python3 -m pip install --user tabulate
 python3 -m pip install --user virtualenv
+python3 -m pip install --user wllvm
 
 gem install --user bundler

011363848083b8b41ab18e9085abaed5300574a0
Bruce Collie <brucecollie82@gmail.com>
2024-09-05 20:33:31 +0100

    No more pip

--- install-packages.sh
+++ install-packages.sh
@@ -1,15 +1,3 @@
 brew update
 brew upgrade
 brew bundle
-
-python3 -m pip install --user --upgrade pip
-python3 -m pip install --user \
-  Pygments      \
-  candl-lexer   \
-  graphviz      \
-  hole-c-lexer  \
-  lit           \
-  pymupdf       \
-  tabulate      \
-  virtualenv    \
-  wllvm
